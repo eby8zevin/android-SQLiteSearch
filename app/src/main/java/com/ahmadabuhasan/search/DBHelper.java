6dfa560d26ae735b9c0f9acb27fecb916f531dbd
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-12 23:23:05 +0700

    SQLITE DBHelper

--- app/src/main/java/com/ahmadabuhasan/search/DBHelper.java
+++ app/src/main/java/com/ahmadabuhasan/search/DBHelper.java
@@ -1,61 +1,46 @@
 package com.ahmadabuhasan.search;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by Ahmad Abu Hasan on 30/11/2020, 25/02/2021
+ * Created by Ahmad Abu Hasan on 30/11/2020, 25/02/2021, 12/02/2022
  */
 
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
 
-    public static final String DATABASE_NAME = "skripsi.db";
+    public static final String DATABASE_NAME = "search.db";
 
-    public static final String TABLE_BARANG = "barang";
+    public static final String TABLE_DATA = "data";
 
-    public static final String BARANG_ID = "id";
-    public static final String BARANG_NAMA = "NamaBarang";
-    public static final String BARANG_BELI = "Beli";
-    public static final String BARANG_STOCK = "Stock";
-    public static final String BARANG_HARGA = "Harga";
+    public static final String DATA_ID = "DataId";
+    public static final String DATA_NAME = "NameProduct";
+    public static final String DATA_BUY = "Buy";
+    public static final String DATA_STOCK = "Stock";
+    public static final String DATA_PRICE = "Price";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    private static final String CREATE_TABLE_BARANG = "CREATE TABLE " + TABLE_BARANG +
-            "(" + BARANG_ID + " INTEGER PRIMARY KEY,"
-            + BARANG_NAMA + " TEXT,"
-            + BARANG_BELI + " TEXT,"
-            + BARANG_STOCK + " TEXT,"
-            + BARANG_HARGA + " TEXT"
-            + ")";
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        db.execSQL(CREATE_TABLE_BARANG);
-        db.execSQL("INSERT INTO " + TABLE_BARANG + "(BARANG_ID, BARANG_NAMA, BARANG_BELI, BARANG_STOCK, BARANG_HARGA) 
-                   VALUES 
-                   (1, 'Kuas', '1000', '10', '2000')");
-        db.execSQL("INSERT INTO " + TABLE_BARANG + "(BARANG_ID, BARANG_NAMA, BARANG_BELI, BARANG_STOCK, BARANG_HARGA) 
-                   VALUES 
-                   (2, 'Paku', '3000', '20', '4000')");
-        db.execSQL("INSERT INTO " + TABLE_BARANG + "(BARANG_ID, BARANG_NAMA, BARANG_BELI, BARANG_STOCK, BARANG_HARGA) 
-                   VALUES 
-                   (3, 'Cat', '5000', '30', '6000')");
-
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_DATA +
+                "(DataId INTEGER PRIMARY KEY, NameProduct TEXT, Buy TEXT, Stock TEXT, Price TEXT)");
+        db.execSQL("INSERT INTO " + TABLE_DATA +
+                "(DataId, NameProduct, Buy, Stock, Price) VALUES (1, 'Facebook', '111', '11','1111')");
+        db.execSQL("INSERT INTO " + TABLE_DATA +
+                "(DataId, NameProduct, Buy, Stock, Price) VALUES (2, 'Instagram', '222', '22','2222')");
+        db.execSQL("INSERT INTO " + TABLE_DATA +
+                "(DataId, NameProduct, Buy, Stock, Price) VALUES (3, 'WhatsApp', '333', '33','3333')");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BARANG);
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DATA);
+        onCreate(db);
     }
- 
 }
